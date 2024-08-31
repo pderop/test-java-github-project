6923c9aa7acfcd4f969e6005b96750bb318b005e
Pierre De Rop <pierre.derop@gmail.com>
2024-08-31 16:45:58 +0200

    test

--- fancy-library-name/src/main/java/org/acme/fancy/lib/template/JavaLibraryTemplate.java
+++ fancy-library-name/src/main/java/org/acme/fancy/lib/template/JavaLibraryTemplate.java
@@ -1,5 +1,11 @@
 package org.acme.fancy.lib;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.sql.PreparedStatement;
+
 /**
  * Javadoc for {@link JavaLibraryTemplate}
  */
@@ -24,6 +30,32 @@ public class JavaLibraryTemplate {
         }
 
         return new String(chars);
+    }
+
+    void testCodeQL(String userInput) throws Exception {
+            // Establishing a connection to the database
+            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/testdb", "root", "password");
+
+            // Vulnerable code: SQL Injection
+            String query = "SELECT * FROM users WHERE username = '" + userInput + "'";
+            Statement stmt = connection.createStatement();
+            ResultSet rs = stmt.executeQuery(query);
+
+            // Process the results
+            while (rs.next()) {
+                System.out.println("User: " + rs.getString("username"));
+            }
+
+            // Secure code: Using PreparedStatement to prevent SQL Injection
+            String secureQuery = "SELECT * FROM users WHERE username = ?";
+            PreparedStatement pstmt = connection.prepareStatement(secureQuery);
+            pstmt.setString(1, userInput);
+            ResultSet secureRs = pstmt.executeQuery();
+
+            // Process the secure results
+            while (secureRs.next()) {
+                System.out.println("Secure User: " + secureRs.getString("username"));
+            }
     }
 
 }
